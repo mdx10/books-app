923d9225ceb6b15b7b8feb3023ae783e182d2136
Aleksandr Smirnov <smirnov4970@gmail.com>
2024-01-20 22:17:43 +0700

    add on-change

--- src/app/app.js
+++ src/app/app.js
@@ -4,6 +4,11 @@ export class App {
   routes = [
     { path: '', view: MainView },
   ];
+
+  appState = {
+    favorites: [],
+  };
+
   constructor() {
     document.addEventListener('hashchange', this.route.bind(this));
     this.route();
@@ -14,7 +19,7 @@ export class App {
       this.currentView.destroy();
     }
     const view = this.routes.find(route => route.path === location.hash).view;
-    this.currentView = new view();
+    this.currentView = new view(this.appState);
     this.currentView.render();
   }
 }
--- src/views/main/main.js
+++ src/views/main/main.js
@@ -1,14 +1,30 @@
+import onChange from 'on-change';
 import { AbstractView } from '../../common/view';
 
 export class MainView extends AbstractView {
-  constructor() {
+  state = {
+    list: [],
+    searchQuery: undefined,
+    offset: 0,
+    loading: false,
+  };
+
+  constructor(appState) {
     super();
+    this.appState = appState;
+    this.appState = onChange(this.appState, this.appStateHook.bind(this));
     this.setTitle('Поиск книг');
+  }
+
+  appStateHook(path) {
+    if (path === 'favorites') {
+      console.log(path);
+    }
   }
 
   render() {
     const main = document.createElement('div');
-    main.innerHTML = 'Test';
+    main.innerHTML = `Число книг: ${this.appState.favorites.length}`;
     this.app.innerHTML = '';
     this.app.append(main);
   }
